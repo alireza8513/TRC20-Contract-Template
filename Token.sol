94df5cc79b474fa4a9bcb6c3724fee3c9b6fc494
alireza8513 <87155019+alireza8513@users.noreply.github.com>
2021-07-09 04:21:37 +0430

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("tusha", "tus",T 18) {
+    constructor () public ERC20Detailed("tusha", "tus", 18) {
         _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
     }
 }
